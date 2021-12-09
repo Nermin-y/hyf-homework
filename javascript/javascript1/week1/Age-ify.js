2bfe1bdaf7034fce46d826f5ea8c4e95a8e915bd
nermin <nerminyahya1986@hotmail.com>
2021-12-09 01:31:41 +0100

    Age-ify.js

---
+++ javascript/javascript1/week1/Age-ify.js
@@ -0,0 +1,5 @@
+// A future age calculator
+const yearOfBirth = 1987; /* this variable shouldn’t change */
+let yearFuture = 2027; /* this variable can be changed */
+let age = yearFuture - yearOfBirth;
+console.log("You will be " + age + " years old in " + yearFuture + ".")
