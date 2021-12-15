0bbc87791b89d2adb11e1050e0b28548b2ec0add
nermin <nerminyahya1986@hotmail.com>
2021-12-15 22:43:16 +0100

    Goodboy-Oldboy

--- javascript/javascript1/week1/Goodboy-Oldboy.js
+++ javascript/javascript1/week1/Goodboy-Oldboy.js
@@ -1,18 +1,21 @@
-
 //Goodboy-Oldboy (A dog age calculator)
 
 let dogYearOfBirth = 2010;
 let dogYearFuture = 2027;
-let dogYear=dogYearFuture-dogYearOfBirth;
-let dogOld=dogYear*7 
-let humanYears=dogOld/7
-let shouldShowResultInDogYears =true;//boolean
-if (shouldShowResultInDogYears=== true) {
-  console.log( "Your dog will be " + dogOld + " dog years old in " + dogYearFuture + "."
+let dogYear = dogYearFuture - dogYearOfBirth;
+let dogOld = dogYear * 7;
+let humanYears = dogOld / 7;
+let shouldShowResultInDogYears = true; //boolean
+if (shouldShowResultInDogYears === true) {
+  console.log(
+    "Your dog will be " + dogOld + " dog years old in " + dogYearFuture + "."
   );
-  
 } else {
-   ( "Your dog will be " +  humanYears + " human years old in " + dogYearFuture + "."
+  console.log(
+    "Your dog will be " +
+      humanYears +
+      " human years old in " +
+      dogYearFuture +
+      "."
   );
-  
 }
