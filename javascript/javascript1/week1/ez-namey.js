ee2b598db77d9b41591582c3b9f7c5460b2dd0b0
nermin <nerminyahya1986@hotmail.com>
2021-12-15 23:06:30 +0100

    ez-namey.js

--- javascript/javascript1/week1/ez-namey.js
+++ javascript/javascript1/week1/ez-namey.js
@@ -1,8 +1,36 @@
 //Ez Namey (Startup name generator)
- const firstWords = [ "Amazing","super ", "Awesome", "Smart", " Only ","Exciting" , "Nice",
-  "Perfect", " Fascinating"," Digital"];
-const secondWords = ["world", "place", "human",  "Solutions",  "company",  "door",  "value",  " Project ",  "pages",
-  " vision "];
+const firstWords = [
+  "Amazing",
+  "super ",
+  "Awesome",
+  "Smart",
+  " Only ",
+  "Exciting",
+  "Nice",
+  "Perfect",
+  " Fascinating",
+  " Digital",
+];
+const secondWords = [
+  "world",
+  "place",
+  "human",
+  "Solutions",
+  "company",
+  "door",
+  "value",
+  " Project ",
+  "pages",
+  " vision ",
+];
 let companyName =
-  firstWords[Math.floor(Math.random() * 10)] +" " + secondWords[Math.floor(Math.random() * 10)];
-console.log("The startup: " + companyName + " contains " + (companyName.length) +" characters")
+  firstWords[Math.floor(Math.random() * firstWords.length)] +
+  " " +
+  secondWords[Math.floor(Math.random() * secondWords.length)];
+console.log(
+  "The startup: " +
+    companyName +
+    " contains " +
+    companyName.length +
+    " characters"
+);
