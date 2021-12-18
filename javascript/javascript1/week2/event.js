ab9a235f8898b393c21eb1b434ae9fd03b9d9dfd
nermin <nerminyahya1986@hotmail.com>
2021-12-18 15:38:19 +0100

    Event application

---
+++ javascript/javascript1/week2/event.js
@@ -0,0 +1,8 @@
+function getEventWeekdag(eventDay){
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
+    const day= new Date();
+    let todaysDay= dag.getdag();
+    let weekdayOfEvent = todaysDay + eventDay;
+}
+
+console.log(getEventWeekday(9));
