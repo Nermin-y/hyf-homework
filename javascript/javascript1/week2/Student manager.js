8fb021eb372e38006d7aac62889eac33e08e1781
nermin <nerminyahya1986@hotmail.com>
2021-12-24 12:17:08 +0100

    finshed Student manager

--- javascript/javascript1/week2/Student manager.js
+++ javascript/javascript1/week2/Student manager.js
@@ -28,17 +28,11 @@ addStudentToClass('Basma');
     addStudentToClass('loucas');
     
     addStudentToClass('Dronning');
+    addStudentToClass('Dronning');
     
     console.log (class07Students);
   console.log (getNumberOfStudents());
 
-    function getEventWeekday(daysFromToday) {
-    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; //array with weekdays
-    const day = new Date();
-    let todaysDay = day.getDay(); //js method which returns eact today's day by it's number begins from 0 Sunday
-    let eventDay = todaysDay + daysFromToday; //calculates number of event day
-    let weekdayOfEvent = eventDay.getDag; //transform number of the event day from 0 to 6
-    console.log("Todays is " + weekdays[todaysDay] + ".");
-    console.log("Event will be on " + weekdays[weekdayOfEvent] + ".");
+   
    
 }
